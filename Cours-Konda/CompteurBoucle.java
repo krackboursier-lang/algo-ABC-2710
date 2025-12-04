30bfa9ad0bbce274a0714cb4753fd4698faf9754
salah <krackboursier@gmail.com>
2025-12-04 14:18:33 +0100

    konda

---
+++ Cours-Konda/CompteurBoucle.java
@@ -0,0 +1,22 @@
+public class CompteurBoucle {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Compteur croissant");
+        for(int i=0; i<=10;i++){
+
+            System.out.println(i);
+
+        }
+        System.out.println("Compteur décroissant");
+       
+       
+        for(int y=10; y>=0;y--){
+
+                        System.out.println(y);
+        }
+
+
+    }
+    
+}
