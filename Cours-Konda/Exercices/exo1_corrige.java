e03f43a67f4727706e89d6dcb26841e6f00fb291
salah <krackboursier@gmail.com>
2025-12-08 09:03:27 +0100

    kon

---
+++ Cours-Konda/Exercices/exo1_corrige.java
@@ -0,0 +1,26 @@
+package algo_ABC_2710.Cours_Konda.Exercices
+import java.util.Scanner; 
+public class exo1_corrige {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Demander à l'utilisateur de saisir les deux entiers
+        System.out.print("Entrez le premier entier (a) : ");
+        int a = scanner.nextInt();
+
+        System.out.print("Entrez le deuxième entier (b) : ");
+        int b = scanner.nextInt();
+
+        // Calculer le résultat de l'addition
+        int resultat = a + b;
+
+        // Afficher les résultats
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("resultat = a + b");
+        System.out.println("resultat vaut " + resultat);
+
+        // Fermer le scanner
+        scanner.close();
+    }
+}
