48479b3cb875d1d2983a8f2a739e23bf147c6a4d
salah <krackboursier@gmail.com>
2025-12-08 08:42:48 +0100

    konda

---
+++ Cours-Konda/Exercices corrigés/exo18 (1).java
@@ -0,0 +1,40 @@
+/* EXO18 : Avec PRINT & SCANNER : 
+ * 
+ * 
+ * Vous souvenez de l'exercice d'algèbre de Boole de la banque ? Une banque vous accorde un prêt immobilier 
+ * si vous répondez aux critères suivants :
+ * 
+ * condition 1 : Avoir un CDI avec un salaire de plus 3000€/ mois
+ * 
+ * condition 2 : Avoir un apport de 25% de la somme demandé
+ * 
+ * condition 3 (Si la condition 1 n'est pas rempli ) : Avoir une autre propriété 
+ * valant minimum 75% du prêt demandé
+ * 
+ * 
+ * 
+ * 
+ *  *  * **********************AFFICHAGE ATTENDU ****************:
+ * 
+ * Quel est le montant de votre prêt ?
+ * 
+ * 150000
+ * 
+  * Quel est votre salaire ?
+ * 
+ * 150000
+ * 
+ * 
+ * Êtes-vous en CDI ?
+ * 
+ * TRUE
+ * 
+ * Combien avez-vous d'apport ?
+ * 
+ * 1500
+ * 
+ * Vous ne pouvez pas hélas obtenir de prêt !
+
+ * 
+*/
+
---
+++ Cours-Konda/Exercices corrigés/exo5 (1).java
@@ -0,0 +1,13 @@
+/* EXO5 : A l'aide de Scanner et Print, créer une équation  A multiplier par B diviser par C (tous types entiers) , récupérer le résultat et vérifier si elle inférieur ou supérieur à 10
+ * 
+ * 
+ * **********************AFFICHAGE ATTENDU ****************:
+ * 
+ * equation = (EQUATION)
+ * 
+ * equation = 15
+ * 
+ * 
+ * equation est superieur
+ * 
+*/
