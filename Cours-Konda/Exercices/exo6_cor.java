7174e6c628959df4f4ee32b00daa18181bfe84c5
salah <krackboursier@gmail.com>
2025-12-08 10:34:56 +0100

    konda

--- Cours-Konda/Exercices/exo6_cor.java
+++ Cours-Konda/Exercices/exo6_cor.java
@@ -1,26 +1,28 @@
-    public static void main(String[] args) { 
+public class exo6_cor{ 
+
+
+
+
+public static void main(String[] args) { 
     
 
-          /***--------  Début du code  -------------------****/
-
-          /* Déclaration des variables */
+          
           String salah;
           int 45;
 
 
-          Scanner clavier = new Scanner(System.in); // Déclaration du scanner Instance de l'Objet Scanner 
+          scanner clavier = new Scanner(System.in); // Déclaration du scanner Instance de l'Objet Scanner 
 
           System.out.println("Quel est votre prénom ?"); // Affiche un message
-          prenom = clavier.nextLine(); // On affecte une valeur Scanner de type text à prénom
+          salah = clavier.nextLine(); // On affecte une valeur Scanner de type text à prénom
           System.out.println("Quel est votre âge");// Affiche un message
-          age = clavier.nextInt();// On affecte une valeur Scanner de type entier à âge
+          45 = clavier.nextInt();// On affecte une valeur Scanner de type entier à âge
           
-          System.out.println("Bonjour, je m'appelle "+prenom+" j'ai "+age+" ans"); /* Afficher votre instruction , concaténation pour la phrase dynamique */
+          System.out.println("Bonjour, je m'appelle "+salah+" j'ai "+45+" ans"); /* Afficher votre instruction , concaténation pour la phrase dynamique */
 
         clavier.close();
 
-        /***--------  Fin du code  -------------------****/
+        
 
     }
 
-}
