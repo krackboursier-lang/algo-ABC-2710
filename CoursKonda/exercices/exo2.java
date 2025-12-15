e4e8829032ae904b41ecbf2b15c7fbb82311f2a0
salah <krackboursier@gmail.com>
2025-12-15 08:18:38 +0100

    konda

--- CoursKonda/exercices/exo2.java
+++ CoursKonda/exercices/exo2.java
@@ -1,23 +1,3 @@
-
-
-/* EXO3 : : A l'aide de Scanner et Print, me faire afficher le résultat d'une soustraction (types entiers) et vérifier si elle est inférieur à 0    
- * 
- * 
- * 
- * **********************AFFICHAGE ATTENDU ****************:
- * 
- * a = 2
- * b = 3
- * 
- * resultat = a - b
- * 
- * resultat = -1
- * 
- * resutat est inférieur à 0
- * 
- * 
- * 
- */
 package exercices;
 import java.util.Scanner;
 import java.io.PrintStream;
