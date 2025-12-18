cd9a46e56b3dc409e3fa23b3e3cf796cae364394
salah <krackboursier@gmail.com>
2025-12-18 12:28:11 +0100

    konda

--- CHATELOT/Exercice A/Exo3_3.java
+++ CHATELOT/Exercice A/Exo3_3.java
@@ -1,26 +1,34 @@
-import java.util.Arrays;
-
+import java.util.Scanner;
 public class Exo3_3 {
-    
-    public static int rechercher(int[] t, int cible) {
-        int d = 0, f = t.length - 1;
-        while (d <= f) {
-            int m = d + (f - d) / 2;
-            if (t[m] == cible) return m;
-            if (t[m] < cible) d = m + 1;
-            else f = m - 1;
+
+    // Méthode pour rechercher un nombre dans un tableau trié
+    public static int rechercher(int[] tableau, int cible) {
+        int debut = 0;
+        int fin = tableau.length - 1;
+
+        while (debut <= fin) {
+            int milieu = debut + (fin - debut) / 2; // Évite l'overflow
+
+            if (tableau[milieu] == cible) {
+                return milieu; // Retourne l'indice si trouvé
+            } else if (tableau[milieu] < cible) {
+                debut = milieu + 1; // Recherche dans la moitié droite
+            } else {
+                fin = milieu - 1; // Recherche dans la moitié gauche
+            }
         }
-        return -1;
+        return -1; // Retourne -1 si non trouvé
     }
 
+    // Méthode principale pour tester
     public static void main(String[] args) {
-        int[] tab = {5, 12, 18, 25, 30, 42, 55, 60, 78, 81};
-        int[] tests = {42, 99};
-        
+        int[] tableau = {5, 12, 18, 25, 30, 42, 55, 60, 78, 81};
+        int[] tests = {42, 99, 5, 18, 100};
+
         for (int n : tests) {
-            int idx = rechercher(tab, n);
-            System.out.println("Recherche " + n + ": " + 
-                (idx != -1 ? "indice " + idx : "404 Not found"));
+            int indice = rechercher(tableau, n);
+            System.out.println("Recherche " + n + ": " +
+                (indice != -1 ? "indice " + indice : "404 Not found"));
         }
     }
 }
