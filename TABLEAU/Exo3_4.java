fe6eb49c4c56787b7c7657fe96d68089aa5b41bf
salah <krackboursier@gmail.com>
2025-12-24 08:11:21 +0100

    chatelot

--- TABLEAU/Exo3_4.java
+++ TABLEAU/Exo3_4.java
@@ -1,46 +1,82 @@
-//import java.util.Scanner;
-//import java.util.Random;
+import java.util.Scanner;
 
+public class Exo3_4 {
 
-import java.util.*;
-public class Exo3_4  {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        boolean trouvePoint = false;
+        boolean trouveLettre = false;
 
-public static void trierAvecDetails(int[] tableau) {
-    int n = tableau.length;
-    
-    for (int i = 0; i < n - 1; i++) {
-        System.out.println("\nÉtape " + (i + 1) + " :");
-        System.out.print("Recherche du minimum dans [");
-        for (int k = i; k < n; k++) {
-            System.out.print(tableau[k] + (k < n - 1 ? ", " : ""));
-        }
-        System.out.println("]");
-        
-        int indiceMin = i;
-        for (int j = i + 1; j < n; j++) {
-            if (tableau[j] < tableau[indiceMin]) {
-                indiceMin = j;
+        int[] occurence;
+
+        char lettre;
+
+        String chaineString;
+
+        do {
+            System.out.println(" Veuillez saisir une phrase terminée par un point !");
+            chaineString = sc.nextLine().toLowerCase();
+            int depart = chaineString.length() - 1;
+            String souString = chaineString.substring(depart);
+            if (souString.equals(".")) {
+                trouvePoint = true;
             }
+
+        } while (trouvePoint == false);
+
+        chaineString = chaineString.replace(" ", "");
+        // String newchaineString = chaineString.replace( "à", "a");
+
+        System.out.println(chaineString);
+        occurence = new int[chaineString.length()];
+
+        System.out.println(" Veuillez saisir une lettre qui sera peut-être dans la phrase :");
+        lettre = sc.nextLine().charAt(0);
+
+        // String chainetab="";
+
+        char[] tabphrase = new char[chaineString.length()];
+
+        for (int i = 0; i < chaineString.length(); i++) {
+
+            tabphrase[i] = chaineString.charAt(i);
         }
-        
-        System.out.println("Minimum trouvé : " + tableau[indiceMin] + " à l'indice " + indiceMin);
-        
-        if (indiceMin != i) {
-            System.out.println("Permutation de " + tableau[i] + " et " + tableau[indiceMin]);
-            int temp = tableau[i];
-            tableau[i] = tableau[indiceMin];
-            tableau[indiceMin] = temp;
+
+        System.out.print("[ ");
+        for (char c : tabphrase) {
+            System.out.print(c + " ");
         }
-        
-        System.out.print("Tableau actuel : ");
-        afficherTableau(tableau);
+
+        System.out.print("] ");
+        int position = 0;
+
+        for (int i = 0; i < tabphrase.length; i++) {
+            if (tabphrase[i] == lettre) {
+                occurence[position] = i + 1;
+
+                trouveLettre = true;
+
+                position++;
+
+            }
+
+        }
+
+        if (trouveLettre == false) {
+            System.out.println("la lettre n'est pas présente  dans la phrase");
+        } else {
+
+            System.out.println(" la lettre est présente dans le tableau de char aux positions suivantes :");
+            for (int value : occurence) {
+                if (value != 0) {
+                    System.out.print(value + " ");
+
+                }
+
+            }
+
+        }
+        sc.close();
     }
 }
-
- private static void afficherTableau(int[] tableau) {
-    
-    throw new UnsupportedOperationException("'afficherTableau'");
- }  
-
-
-}
+ 
